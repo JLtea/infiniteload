73a9ea52927a7930a7abd13f8a1f4d5b6f86a62c
jtlee <jtlee@DESKTOP-U33FJE7.localdomain>
2020-11-06 18:00:35 +0900

    styled

--- app/src/components/InfiniteLoader.js
+++ app/src/components/InfiniteLoader.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Spin, Card } from "antd";
+import { LoadingOutlined, ReloadOutlined } from "@ant-design/icons";
+import { Container, CardLayout, Loader } from "./styles";
 
-//Mock API call object
-const mock = {};
+//Mock API call
+const url = "https://jsonplaceholder.typicode.com";
+
 const InfiniteLoader = () => {
-// Card State
-    const [cards, setCards] = useState([]);
-// Infinite Loader State
+  // Card State
+  const [cards, setCards] = useState([]);
+  // Infinite Loader State
   const [trackPage, setTrackPage] = useState(1);
   const [infiniteLoad, setInfiniteLoad] = useState(true);
   const loader = useRef(null);
@@ -17,10 +21,18 @@ const InfiniteLoader = () => {
     }
   };
   const fetchCards = async () => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 2000);
-    });
-    setData(mock);
+    // await new Promise((resolve) => {
+    //   setTimeout(resolve, 2000);
+    // });
+    //set fetched data
+    const response = await fetch(`${url}/posts`);
+    const json = await response.json()
+    console.log(json);
+    setCards((current) => [
+      ...current,
+      json.splice(cards.length, cards.length + 10),
+    ]);
+    //set spinner off
     setInfiniteLoad(false);
   };
 
@@ -37,32 +49,29 @@ const InfiniteLoader = () => {
   }, []);
 
   useEffect(() => {
+    //set spinner on
     setInfiniteLoad(true);
     // make API call here
     fetchCards();
   }, [trackPage]);
+
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          alignContent: "center",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-          height: "200px",
-        }}
-      >
+    <Container>
+      <CardLayout>
+        {cards.map((card) => (
+          <Card style={{width: 400, height: 400}} title={card.title}>{card.body}</Card>
+        ))}
+      </CardLayout>
+      <Loader className="infiniteLoad" ref={loader}>
         {infiniteLoad ? (
-          <Spin
-            indicator={<Icon type="loading" style={{ fontSize: 50 }} spin />}
-          />
+          <Spin indicator={<LoadingOutlined style={{ fontSize: 50 }} />} />
         ) : (
-          <h2 style={{ color: "#d9d9d9" }}>Load More</h2>
+          <h2 style={{ color: "#d9d9d9" }}>
+            Load More <ReloadOutlined />
+          </h2>
         )}
-      </div>
-      <span className="infiniteLoad" ref={loader}></span>
-    </>
+      </Loader>
+    </Container>
   );
 };
 
---
+++ app/src/components/styles.js
@@ -0,0 +1,24 @@
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #f0f0f0;
+`;
+
+export const CardLayout = styled.div`
+    width: 80%;
+    display: flex;
+    flex-wrap: wrap;
+
+`;
+
+export const Loader = styled.div`
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: "200px;
+`;
